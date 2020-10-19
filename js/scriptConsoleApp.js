6385609b5ec25788bb22ffe3cb6f3270e5e1138d
Sergii Grynevych <grinevich.sergey.official@gmail.com>
2020-10-19 17:36:44 +0300

    first result of task

---
+++ js/scriptConsoleApp.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const latestSeenFilmsByUser = prompt('Один из последних просмотренных фильмов?', '');
+
+const markOfFilm = prompt('На сколько оцените его?', '');
+
+const movies = {
+    latestSeenFilmsByUser: markOfFilm
+};
