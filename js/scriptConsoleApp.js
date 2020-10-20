d67a3749860618f2373970502a77c3d60910ad62
Sergii Grynevych <grinevich.sergey.official@gmail.com>
2020-10-20 19:55:14 +0300

    add second cycle of task

--- js/scriptConsoleApp.js
+++ js/scriptConsoleApp.js
@@ -10,18 +10,32 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i += 1){
+// first cycle of task
+// for (let i = 0; i < 2; i += 1){
     
+//     const latestSeenFilmsByUser = prompt('Один из последних просмотренных фильмов?', ''),
+//           markOfFilm = +prompt('На сколько оцените его?', '');
+
+//     if (latestSeenFilmsByUser != null && markOfFilm != null && latestSeenFilmsByUser != '' && markOfFilm != '' && latestSeenFilmsByUser.length < 50) {
+//             personalMovieDB.movies[latestSeenFilmsByUser] = markOfFilm;
+//     } else {
+//         i--;
+//     }
+// }
+
+// second cycle of task
+let i = 0;
+
+do {
     const latestSeenFilmsByUser = prompt('Один из последних просмотренных фильмов?', ''),
           markOfFilm = +prompt('На сколько оцените его?', '');
-        
+
     if (latestSeenFilmsByUser != null && markOfFilm != null && latestSeenFilmsByUser != '' && markOfFilm != '' && latestSeenFilmsByUser.length < 50) {
             personalMovieDB.movies[latestSeenFilmsByUser] = markOfFilm;
     } else {
         i--;
     }
-
-}
+} while (i++ < 1);   
 
 if (personalMovieDB.count < 10) {
     alert("Просмотрено довольно мало фильмов");
